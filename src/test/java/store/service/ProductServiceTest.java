60cdab7f4ef2eced994c6ce536317b8a8829c3b4
ajroot5685 <anjeong8577@naver.com>
2024-11-09 01:42:38 +0900

    test(printProductInfo): 상품 정보 출력 테스트

--- src/test/java/store/service/ProductServiceTest.java
+++ src/test/java/store/service/ProductServiceTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import store.builder.ProductOutputBuilder;
 import store.entity.Product;
 import store.file.ConvenienceDataReader;
 import store.model.ProductModel;
@@ -21,11 +22,11 @@ class ProductServiceTest {
         productModel = new ProductModel();
 
         productService = new ProductService("products.md", new ConvenienceDataReader(), new ProductDtoParser(),
-                productModel);
+                new ProductOutputBuilder(), productModel);
     }
 
     @Test
-    void 제품_정보를_불러와_저장한다() {
+    void 상품_정보를_불러와_저장한다() {
         // given
 
         // when
@@ -34,5 +35,18 @@ class ProductServiceTest {
         // then
         Map<String, Product> products = productModel.getProducts();
         assertThat(products.values().size()).isGreaterThan(0);
+    }
+
+    @Test
+    void 저장된_상품_정보를_StringBuilder_에_추가한다() {
+        // given
+        productService.supply();
+        StringBuilder sb = new StringBuilder();
+
+        // when
+        productService.appendProductInfo(sb);
+
+        // then
+        assertThat(!sb.isEmpty()).isTrue();
     }
 }
